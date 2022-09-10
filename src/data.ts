434bf24976210418f6d789ca164f33cf14944d16
KatTheFox <katthefoxmaid@gmail.com>
2022-09-10 13:46:04 -0400

    feat: added compatible lore shelf for More Shelves

--- src/data.ts
+++ src/data.ts
@@ -489,6 +489,353 @@ const lore: Shelf = {
     },
   ],
 };
+const loreNoSH: Shelf = {
+  name: "Lore (No Secret Histories)",
+  category: "lore",
+  id: "kat.lore.compat",
+  rows: 8,
+  columns: 8,
+  areas: [
+    {
+      expression: "[fragmentknock]",
+      x: 1,
+      y: 2,
+    },
+    {
+      expression: "[fragmentknockb]",
+      x: 1,
+      y: 3,
+    },
+    {
+      expression: "[fragmentknockc]",
+      x: 1,
+      y: 4,
+    },
+    {
+      expression: "[fragmentknockd]",
+      x: 1,
+      y: 5,
+    },
+    {
+      expression: "[fragmentknocke]",
+      x: 1,
+      y: 6,
+    },
+    {
+      expression: "[fragmentknockf]",
+      x: 1,
+      y: 7,
+    },
+    {
+      expression: "[fragmentknockg]",
+      x: 1,
+      y: 8,
+    },
+    {
+      expression: "[fragmentlantern]",
+      x: 2,
+      y: 2,
+    },
+    {
+      expression: "[fragmentlanternb]",
+      x: 2,
+      y: 3,
+    },
+    {
+      expression: "[fragmentlanternc]",
+      x: 2,
+      y: 4,
+    },
+    {
+      expression: "[fragmentlanternd]",
+      x: 2,
+      y: 5,
+    },
+    {
+      expression: "[fragmentlanterne]",
+      x: 2,
+      y: 6,
+    },
+    {
+      expression: "[fragmentlanternf]",
+      x: 2,
+      y: 7,
+    },
+    {
+      expression: "[fragmentlanterng]",
+      x: 2,
+      y: 8,
+    },
+
+    {
+      expression: "[fragmentforge]",
+      x: 3,
+      y: 2,
+    },
+    {
+      expression: "[fragmentforgeb]",
+      x: 3,
+      y: 3,
+    },
+    {
+      expression: "[fragmentforgec]",
+      x: 3,
+      y: 4,
+    },
+    {
+      expression: "[fragmentforged]",
+      x: 3,
+      y: 5,
+    },
+    {
+      expression: "[fragmentforgee]",
+      x: 3,
+      y: 6,
+    },
+    {
+      expression: "[fragmentforgef]",
+      x: 3,
+      y: 7,
+    },
+    {
+      expression: "[fragmentforgeg]",
+      x: 3,
+      y: 8,
+    },
+    {
+      expression: "[fragmentedge]",
+      x: 4,
+      y: 2,
+    },
+    {
+      expression: "[fragmentedgeb]",
+      x: 4,
+      y: 3,
+    },
+    {
+      expression: "[fragmentedgec]",
+      x: 4,
+      y: 4,
+    },
+    {
+      expression: "[fragmentedged]",
+      x: 4,
+      y: 5,
+    },
+    {
+      expression: "[fragmentedgee]",
+      x: 4,
+      y: 6,
+    },
+    {
+      expression: "[fragmentedgef]",
+      x: 4,
+      y: 7,
+    },
+    {
+      expression: "[fragmentedgeg]",
+      x: 4,
+      y: 8,
+    },
+
+    {
+      expression: "[fragmentwinter]",
+      x: 5,
+      y: 2,
+    },
+    {
+      expression: "[fragmentwinterb]",
+      x: 5,
+      y: 3,
+    },
+    {
+      expression: "[fragmentwinterc]",
+      x: 5,
+      y: 4,
+    },
+    {
+      expression: "[fragmentwinterd]",
+      x: 5,
+      y: 5,
+    },
+    {
+      expression: "[fragmentwintere]",
+      x: 5,
+      y: 6,
+    },
+    {
+      expression: "[fragmentwinterf]",
+      x: 5,
+      y: 7,
+    },
+    {
+      expression: "[fragmentwinterg]",
+      x: 5,
+      y: 8,
+    },
+
+    {
+      expression: "[fragmentheart]",
+      x: 6,
+      y: 2,
+    },
+    {
+      expression: "[fragmentheartb]",
+      x: 6,
+      y: 3,
+    },
+    {
+      expression: "[fragmentheartc]",
+      x: 6,
+      y: 4,
+    },
+    {
+      expression: "[fragmentheartd]",
+      x: 6,
+      y: 5,
+    },
+    {
+      expression: "[fragmenthearte]",
+      x: 6,
+      y: 6,
+    },
+    {
+      expression: "[fragmentheartf]",
+      x: 6,
+      y: 7,
+    },
+    {
+      expression: "[fragmentheartg]",
+      x: 6,
+      y: 8,
+    },
+
+    {
+      expression: "[fragmentgrail]",
+      x: 7,
+      y: 2,
+    },
+    {
+      expression: "[fragmentgrailb]",
+      x: 7,
+      y: 3,
+    },
+    {
+      expression: "[fragmentgrailc]",
+      x: 7,
+      y: 4,
+    },
+    {
+      expression: "[fragmentgraild]",
+      x: 7,
+      y: 5,
+    },
+    {
+      expression: "[fragmentgraile]",
+      x: 7,
+      y: 6,
+    },
+    {
+      expression: "[fragmentgrailf]",
+      x: 7,
+      y: 7,
+    },
+    {
+      expression: "[fragmentgrailg]",
+      x: 7,
+      y: 8,
+    },
+    {
+      expression: "[fragmentmoth]",
+      x: 8,
+      y: 2,
+    },
+    {
+      expression: "[fragmentmothb]",
+      x: 8,
+      y: 3,
+    },
+    {
+      expression: "[fragmentmothc]",
+      x: 8,
+      y: 4,
+    },
+    {
+      expression: "[fragmentmothd]",
+      x: 8,
+      y: 5,
+    },
+    {
+      expression: "[fragmentmothe]",
+      x: 8,
+      y: 6,
+    },
+    {
+      expression: "[fragmentmothf]",
+      x: 8,
+      y: 7,
+    },
+    {
+      expression: "[fragmentmothg]",
+      x: 8,
+      y: 8,
+    },
+    {
+      id: "paintingmastersecrethistories",
+      expression: "[paintingmastersecrethistories]",
+      x: 1,
+      y: 1,
+    },
+    {
+      id: "paintingmasterknock",
+      expression: "[paintingmasterknock]",
+      x: 1,
+      y: 1,
+    },
+    {
+      id: "paintingmasterlantern",
+      expression: "[paintingmasterlantern]",
+      x: 2,
+      y: 1,
+    },
+    {
+      id: "paintingmasterforge",
+      expression: "[paintingmasterforge]",
+      x: 3,
+      y: 1,
+    },
+    {
+      id: "paintingmasteredge",
+      expression: "[paintingmasteredge]",
+      x: 4,
+      y: 1,
+    },
+    {
+      id: "paintingmasterwinter",
+      expression: "[paintingmasterwinter]",
+      x: 5,
+      y: 1,
+    },
+    {
+      id: "paintingmasterheart",
+      expression: "[paintingmasterheart]",
+      x: 6,
+      y: 1,
+    },
+    {
+      id: "paintingmastergrail",
+      expression: "[paintingmastergrail]",
+      x: 7,
+      y: 1,
+    },
+    {
+      id: "paintingmastermoth",
+      expression: "[paintingmastermoth]",
+      x: 8,
+      y: 1,
+    },
+  ],
+};
 const rites: Shelf = {
   id: "kat.rites",
   category: "rites",
@@ -967,6 +1314,7 @@ export const shelves = {
     tools,
     rites,
     followers,
+    loreNoSH,
     waysThroughDream,
     apostleLantern,
     pigments,

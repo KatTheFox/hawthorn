c0dafa829df10d3780ed8ec165fce18bf0fc8431
Katrina Renarde <katthefoxmaid@gmail.com>
2023-03-03 20:36:23 -0800

    added a shelf for JW's followers

--- src/shelves/vanilla/followers.ts
+++ src/shelves/vanilla/followers.ts
@@ -17,6 +17,13 @@ export const followers: Shelf = {
       x: 1,
       y: 2,
     },
+
+    {
+      expression:
+        "[colton_a] || [colton_b] || [colton_c] || [colton_r] || [colton_p] || [colton_d]",
+      x: 1,
+      y: 3,
+    },
     {
       expression: "[generic_a_knock] || [generic_b_knock] || [generic_c_knock]",
       x: 1,
@@ -171,6 +178,219 @@ export const followers: Shelf = {
       expression:
         "[ysabet_a] || [ysabet_b] || [ysabet_c] || [ysabet_r] || [ysabet_p] || [ysabet_d]",
       x: 8,
+      y: 1,
+    },
+  ],
+};
+
+export const jwfollowers: Shelf = {
+  id: "kat.jwfollowers",
+  category: "followers",
+  name: "[KS] JW Followers",
+  rows: 4,
+  columns: 9,
+  areas: [
+    {
+      expression:
+        "[cheyenne_a] || [cheyenne_b] || [cheyenne_c] || [cheyenne_r] || [cheyenne_p] || [cheyenne_d]",
+    },
+
+    {
+      expression:
+        "[joshua_a] || [joshua_b] || [joshua_c] || [joshua_r] || [joshua_p] || [joshua_d]",
+      x: 1,
+      y: 2,
+    },
+    {
+      expression:
+        "[jesse_a] || [jesse_b] || [jesse_c] || [jesse_r] || [jesse_p] || [jesse_d]",
+      x: 1,
+      y: 3,
+    },
+    {
+      expression:
+        "[generic_a_secrethistories] || [generic_b_secrethistories] || [generic_c_secrethistories]",
+      x: 1,
+      y: 4,
+    },
+    {
+      expression:
+        "[neville_a] || [neville_b] || [neville_c] || [neville_r] || [neville_p] || [neville_d]",
+      x: 2,
+      y: 1,
+    },
+    {
+      expression:
+        "[enid_a] || [enid_b] || [enid_c] || [enid_r] || [enid_p] || [enid_d]",
+      x: 2,
+      y: 2,
+    },
+    {
+      expression: "[generic_a_knock] || [generic_b_knock] || [generic_c_knock]",
+      x: 2,
+      y: 4,
+    },
+    {
+      expression:
+        "[generic_a_lantern] || [generic_b_lantern] || [generic_c_lantern]",
+      x: 3,
+      y: 4,
+    },
+    {
+      expression: "[generic_a_forge] || [generic_b_forge] || [generic_c_forge]",
+      x: 3,
+      y: 4,
+    },
+    {
+      expression: "[generic_a_edge] || [generic_b_edge] || [generic_c_edge]",
+      x: 5,
+      y: 4,
+    },
+    {
+      expression:
+        "[generic_a_winter] || [generic_b_winter] || [generic_c_winter]",
+      x: 6,
+      y: 4,
+    },
+    {
+      expression: "[generic_a_heart] || [generic_b_heart] || [generic_c_heart]",
+      x: 7,
+      y: 4,
+    },
+    {
+      expression: "[generic_a_grail] || [generic_b_grail] || [generic_c_grail]",
+      x: 8,
+      y: 4,
+    },
+    {
+      expression: "[generic_a_moth] || [generic_b_moth] || [generic_c_moth]",
+      x: 9,
+      y: 4,
+    },
+    {
+      expression:
+        "[cat_a] || [cat_b] || [cat_c] || [cat_r] || [cat_p] || [cat_d]",
+      x: 3,
+      y: 1,
+    },
+    {
+      expression:
+        "[clifton_a] || [clifton_b] || [clifton_c] || [clifton_r] || [clifton_p] || [clifton_d]",
+      x: 3,
+      y: 2,
+    },
+    {
+      expression:
+        "[slee_a] || [slee_b] || [slee_c] || [slee_r] || [slee_p] || [slee_d]",
+      x: 3,
+      y: 3,
+    },
+    {
+      expression:
+        "[laidlaw_a] || [laidlaw_b] || [laidlaw_c] || [laidlaw_r] || [laidlaw_p] || [laidlaw_d]",
+      x: 3,
+      y: 1,
+    },
+    {
+      expression:
+        "[tristan_a] || [tristan_b] || [tristan_c] || [tristan_r] || [tristan_p] || [tristan_d]",
+      x: 3,
+      y: 2,
+    },
+    {
+      expression:
+        "[valciane_a] || [valciane_b] || [valciane_c] || [valciane_r] || [valciane_p] || [valciane_d]",
+      x: 3,
+      y: 3,
+    },
+    {
+      expression:
+        "[elridge_a] || [elridge_b] || [elridge_c] || [elridge_r] || [elridge_p] || [elridge_d]",
+      x: 5,
+      y: 3,
+    },
+    {
+      expression:
+        "[rose_a] || [rose_b] || [rose_c] || [rose_r] || [rose_p] || [rose_d]",
+      x: 5,
+      y: 1,
+    },
+    {
+      expression:
+        "[victor_a] || [victor_b] || [victor_c] || [victor_r] || [victor_p] || [victor_d]",
+      x: 5,
+      y: 2,
+    },
+    {
+      expression:
+        "[auclair_a] || [auclair_b] || [auclair_c] || [auclair_r] || [auclair_p] || [auclair_d]",
+      x: 6,
+      y: 1,
+    },
+    {
+      expression:
+        "[violet_a] || [violet_b] || [violet_c] || [violet_r] || [violet_p] || [violet_d]",
+      x: 6,
+      y: 2,
+    },
+    {
+      expression:
+        "[brayden_a] || [brayden_b] || [brayden_c] || [brayden_r] || [brayden_p] || [brayden_d]",
+      x: 6,
+      y: 3,
+    },
+    {
+      expression:
+        "[clovette_a] || [clovette_b] || [clovette_c] || [clovette_r] || [clovette_p] || [clovette_d]",
+      x: 7,
+      y: 1,
+    },
+    {
+      expression:
+        "[dorothy_a] || [dorothy_b] || [dorothy_c] || [dorothy_r] || [dorothy_p] || [dorothy_d]",
+      x: 7,
+      y: 2,
+    },
+    {
+      expression:
+        "[leo_a] || [leo_b] || [leo_c] || [leo_r] || [leo_p] || [leo_d]",
+      x: 7,
+      y: 3,
+    },
+    {
+      expression:
+        "[karina_a] || [karina_b] || [karina_c] || [karina_r] || [karina_p] || [karina_d]",
+      x: 8,
+      y: 1,
+    },
+    {
+      expression:
+        "[renira_a] || [renira_b] || [renira_c] || [renira_r] || [renira_p] || [renira_d]",
+      x: 8,
+      y: 2,
+    },
+    {
+      expression:
+        "[saliba_a] || [saliba_b] || [saliba_c] || [saliba_r] || [saliba_p] || [saliba_d]",
+      x: 8,
+      y: 3,
+    },
+    {
+      expression:
+        "[porter_a] || [porter_b] || [porter_c] || [porter_r] || [porter_p] || [porter_d]",
+      x: 9,
+      y: 3,
+    },
+    {
+      expression:
+        "[sylvia_a] || [sylvia_b] || [sylvia_c] || [sylvia_r] || [sylvia_p] || [sylvia_d]",
+      x: 9,
+      y: 2,
+    },
+    {
+      expression:
+        "[ysabet_a] || [ysabet_b] || [ysabet_c] || [ysabet_r] || [ysabet_p] || [ysabet_d]",
+      x: 9,
       y: 1,
     },
   ],

90918930a0f94e38d30ffad47d85f12214e73189
Katrina Renarde <katthefoxmaid@gmail.com>
2023-12-16 18:12:26 -0800

    fix: framework upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,28 @@
 // This is the configuration file for ESLint, the TypeScript linter.
 // https://eslint.org/docs/user-guide/configuring
 module.exports = {
-    extends: [
-      // The linter base is the shared IsaacScript config.
-      // https://github.com/IsaacScript/isaacscript/blob/main/packages/eslint-config-isaacscript/base.js
-      "eslint-config-isaacscript/base",
-    ],
-  
-    parserOptions: {
-      // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
-      // things to lint correctly. We do not point this at "./tsconfig.json" because certain files
-      // (such at this file) should be linted but not included in the actual project output.
-      project: "./tsconfig.eslint.json",
-    },
-  
-    // We modify the linting rules from the base for some specific things.
-    rules: { "no-bitwise": 0, "no-param-reassign": { props: "false" } },
-  };
-  
+  extends: [
+    // The linter base is the shared IsaacScript config.
+    // https://github.com/IsaacScript/isaacscript/blob/main/packages/eslint-config-isaacscript/base.js
+    "eslint-config-isaacscript/base",
+  ],
+
+  parserOptions: {
+    // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
+    // things to lint correctly. We do not point this at "./tsconfig.json" because certain files
+    // (such at this file) should be linted but not included in the actual project output.
+    project: "./tsconfig.eslint.json",
+  },
+
+  // We modify the linting rules from the base for some specific things.
+  rules: {
+    "no-bitwise": 0,
+    "no-param-reassign": ["error", { props: false }],
+    curly: ["error", "multi"],
+    "n/file-extension-in-import": "off",
+    "no-void": "off",
+    "isaacscript/no-void-return-type": "off",
+    "unicorn/prefer-top-level-await": "off",
+    "isaacscript/complete-sentences-line-comments": "off",
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { writeFileSync } from "fs-extra";
 import { shelves } from "./data";
+import { homedir, machine, type } from "os";
+import { join } from "path";
 
 function updateJson() {
+  if(type()=='Linux')
   writeFileSync(
-    "/home/katrina/.config/unity3d/Weather Factory/Cultist Simulator/mods/hawthorn/content/shelves.json",
+    join(homedir(),".config","unity3d","Weather Factory","Cultist Simulator","mods","hawthorn","content","shelves.json"),
+    JSON.stringify(shelves)
+  );
+  if(type()=='Windows')
+  writeFileSync(
+    join(homedir(),"AppData","LocalLow","Weather Factory","Cultist Simulator","mods","hawthorn","content","shelves.json"),
     JSON.stringify(shelves)
   );
 }
